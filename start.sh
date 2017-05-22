76df83ba9deac453cb341ba9d85997c6c98b9d40
Jonny Ervine <jervine@users.noreply.github.com>
2017-05-22 19:49:21 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##
+## Start up script for NZBGet on CentOS docker container
+##
+
+## Initialise any variables being called:
+# Set the correct timezone
+TZ=${TZ:-UTC}
+USER=$USER
+USERUID=$USERUID
+setup=/config/.setup
+
+if [ ! -f "${setup}" ]; then
+  rm -f /etc/localtime
+  cp /usr/share/zoneinfo/$TZ /etc/localtime
+  adduser -u $USERUID $USER
+  sed -i "s/user=/user=$USER/g" /etc/supervisord.d/nzbget.ini
+  touch $setup
+fi
+
+## Start up NZBGet daemon via supervisord
+/usr/bin/supervisord -n -c /etc/supervisord.conf
